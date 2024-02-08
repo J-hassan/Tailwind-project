db18ab3ded1703b1f910b74154231bc82bc6d7cb
j-hassan <imuhammadhassanktk@gmail.com>
2024-02-08 10:54:57 +0500

    new project

---
+++ scirpt.js
@@ -0,0 +1,8 @@
+
+
+let questoin_1 = document.querySelector("#q1");
+let myname = document.querySelector("#name");
+
+questoin_1.addEventListener("click" , () => {
+   myname.classList.toggle("opacity-100");
+})
